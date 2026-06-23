f4c28bbda6c96e9cd13a2ac97ce670966ee1a26d
Retroqd <supulbaddegama@gmail.com>
2026-06-23 23:11:04 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        std::string encoded_string = "";
+
+        for (auto val : strs) {
+            
+            std::string num = std::to_string(val.size());
+            encoded_string = encoded_string + val + num;
+             
+            
+        }
+
+       
+
+        return encoded_string;
+        
+    }
+
+    vector<string> decode(std::string s) {
+
+        vector<std::string> v;
+
+        int index = 0;
+        std::string conc = s[0];
+
+        for (auto val : s) {
+            
+            int x = val - '0';
+            std::cout << x << " ";
+            if ((x) == index) {
+                
+                v.push_back(conc);
+                index = 0;
+                conc = "";
+            } else {
+                index = index + 1;
+                conc = conc + val;
+            }
+        }
+        return v;
+
+    }
+};
