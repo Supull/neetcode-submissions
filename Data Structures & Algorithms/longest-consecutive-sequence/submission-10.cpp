b532b9b9a96d8b64868158b5a20dbb7379aa8946
Retroqd <supulbaddegama@gmail.com>
2026-06-25 20:06:06 +0530

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        set<int> b(nums.begin(), nums.end());
+        vector<int> s(b.begin(), b.end());
+        vector <int> v;
+        priority_queue<int> pq;
+
+        for (auto val : s) {
+            cout << val << " ";
+        }
+
+        
+        int len = 1;
+        int index = 0;
+        if (nums.size() < 1) {
+            return 0;
+        }
+        pq.push(1);
+        if (nums.size() == 1) {
+            return len;
+        }
+        for (int i = 0; i < s.size()-1; i++) {
+            if ((s[i+1] - s[i]) == 1) {
+                len += 1;
+                v.push_back(len);
+            } else {
+                len = 1;
+            }
+        }
+
+        int max = 0;
+            
+        
+        for (auto val : v) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
+        
+    } 
+};
