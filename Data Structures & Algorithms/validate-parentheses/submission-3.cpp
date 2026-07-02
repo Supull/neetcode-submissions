db8893b260b48f6f50b968c1f6e3356990faaa5b
Retroqd <supulbaddegama@gmail.com>
2026-07-02 19:42:51 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> s2;
+
+        for (char x : s) {
+        if (x == '(' || x == '[' || x == '{') {
+            s2.push(x);
+        }
+        else {
+            if (!s2.empty() &&
+            ((x == ')' && s2.top() == '(') ||
+                (x == ']' && s2.top() == '[') ||
+                (x == '}' && s2.top() == '{'))) {
+                s2.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    
+
+        if (s2.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+        
+    }
+};
