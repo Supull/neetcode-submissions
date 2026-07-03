540c122c7246e6778996228fb5262560ef082933
Retroqd <supulbaddegama@gmail.com>
2026-07-03 17:43:56 +0530

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        stack<int> s1;
+        stack<int> s2;
+
+        int total = 0;
+
+        for (auto val : tokens) {
+
+            if ((val == "+" || val == "*" || val == "/" || val == "-") && s1.size() >= 2) {
+                if (val == "+") {
+                    total = 0;
+                    total = s1.top() + total;
+                    s1.pop();
+                    total = s1.top() + total;
+                    s1.pop();
+                    
+                }
+                else if (val == "-") {
+                    total = 0;
+                    total = s1.top() + total;
+                    s1.pop();
+                    total = s1.top() + total;
+                    s1.pop();
+                    
+                }
+                else if (val == "/") {
+                    total = 1;
+                    total = s1.top() + total;
+                    s1.pop();
+                    total = s1.top() + total;
+                    s1.pop();
+                }
+                else if (val == "*") {
+                    total = 1;
+                    total = s1.top() * total;
+                    s1.pop();
+                    total = s1.top() * total;
+                    s1.pop();
+                    
+                }
+
+                s1.push(total);
+                
+                cout << s1.top();
+
+            } else {
+
+                int x = stoi(val);
+                s1.push(x);
+            }
+
+
+        }
+
+        return s1.top();
+
+
+        
+    }
+};
