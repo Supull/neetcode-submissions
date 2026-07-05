131003dbcdb862f3ef2d26c49c95508ada5e19b1
Retroqd <supulbaddegama@gmail.com>
2026-07-05 23:41:42 +0530

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        double rd = 0;
+        double rs = 0;
+
+        vector<pair<double, double>> v;
+        unordered_map<double,int> mp; 
+        
+
+
+        for (int i = 0; i < speed.size(); i++) {
+
+            rd = target - position[i];
+            rs = rd/speed[i];
+
+            v.first.push_back(rs);
+            v.second.push_back(position[i]);
+
+        }
+
+        for (int i = 0; i < speed.size(); i++) {
+
+            cout << v.first[i] << "," << v.second[i];
+
+        }
+
+        for (auto val : v) {
+
+            mp[val]++;
+
+        }
+
+        int count = 0;
+        cout << endl;
+
+        for (auto val : mp) {
+            cout << val.first << " " << val.second << "v ";
+            count = count + 1;
+        }
+
+        return count;
+        
+    }
+};
