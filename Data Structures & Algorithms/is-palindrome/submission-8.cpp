32c8f642d319288ee34c48eccc001f32d5719524
Retroqd <supulbaddegama@gmail.com>
2026-08-05 17:43:56 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int start = 0;
+        int end = s.size()-1;
+
+        while (start < end) {
+
+            while (end >= 0 && !((s[end] >= 'a' && s[end] <= 'z') ||
+                     (s[end] >= 'A' && s[end] <= 'Z') ||
+                     (s[end] >= '0' && s[end] <= '9'))) {
+                end--;
+            }
+
+            while (start <= s.size()-1 && !((s[start] >= 'a' && s[start] <= 'z') ||
+                     (s[start] >= 'A' && s[start] <= 'Z') ||
+                     (s[start] >= '0' && s[start] <= '9'))) {
+                start++;
+            }
+            
+            if (tolower(s[start]) != tolower(s[end])) {
+                cout << s[start] << "%" << endl;
+                cout << s[end];
+                return false;
+            } else {
+
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+};
