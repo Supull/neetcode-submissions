26a67997d78c93f0892f0fd8d45e25dd144ecb48
Retroqd <supulbaddegama@gmail.com>
2026-08-08 20:17:32 +0530

    Add: trapping-rain-water - submission-23

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-23.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int l = 0;
+        int m = 0;
+        int templ = 0;
+        int r = 1;
+        int area = 0;
+        bool status = false;
+
+        if (height.empty()) return 0;   
+
+        while (l < height.size() && height[l] == 0) {
+            l++;
+            r++;
+            
+        }
+
+        if (r >= height.size()) return 0;
+        
+
+
+        //cout << height[l] << " " << height[r] << endl;
+
+        while (r < height.size()) {
+
+            if (height[l] > height[r]) {
+                //cout << height[l] << " " << height[r] << "yp " << endl;
+                r++;
+                if (r >= height.size()) {
+                    break;
+                }
+                
+            } else { 
+                //cout << height[l] << " " << height[r] << endl;
+                templ = l;
+                l++;
+                while (l < r) {
+                    //cout << height[l] << " " << height[r] << endl;
+                    area = area + (height[templ] - height[l]);
+                    l++;
+                }
+                r++;
+
+                if (r < height.size()) {
+                    //cout << height[l] << " " << height[r] << endl;
+                }
+                
+            }
+
+        }
+
+        return area;
+        
+    }
+};
